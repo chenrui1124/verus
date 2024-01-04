9a6b7dc8924eebbea7328ee69294e29b4f7dd0ef
Rui <75087533+chenrui1124@users.noreply.github.com>
2024-01-04 15:58:13 +0800

    refactor(Switch): update onChange & styles

--- lib/components/switch/VSwitch.vue
+++ lib/components/switch/VSwitch.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { ObjectDirective } from 'vue'
 import type { SwitchModel, SwitchProps } from '.'
 
 defineOptions({ name: 'Switch', inheritAttrs: false })
@@ -9,11 +10,18 @@ const modelValue = defineModel<SwitchModel['modelValue']>({ required: true })
 
 const emit = defineEmits<{ switch: [newValue: boolean] }>()
 
+const vInit: ObjectDirective = {
+  beforeMount(el: HTMLInputElement) {
+    el.checked = !!modelValue.value
+  }
+}
+
 function onChange(e: Event) {
   const t = e.target as HTMLInputElement
+
   requestAnimationFrame(() => {
     modelValue.value = t.checked
-    emit('switch', modelValue.value)
+    emit('switch', modelValue.value!)
   })
 }
 </script>
@@ -22,20 +30,22 @@ function onChange(e: Event) {
   <label
     class="group/v-switch relative inline-block h-7 w-12 cursor-pointer rounded-full transition-colors duration-300 has-[:focus-visible]:v-outline has-[:disabled]:v-disabled"
     :class="[
-      'before:v-shade before:border before:border-solid before:transition-colors before:duration-300',
-      'after:pointer-events-none after:absolute after:inset-y-1 after:left-1 after:-m-2 after:size-9 after:rounded-inherit after:transition after:duration-300 after:ease-braking focus-within:after:!bg-on-bsc/12 hover:after:bg-on-bsc/8',
+      'before:v-shade before:border before:border-solid before:transition-colors before:duration-inherit',
+      'after:pointer-events-none after:absolute after:inset-y-1 after:left-1 after:-m-2 after:size-9 after:rounded-inherit after:transition after:duration-inherit after:ease-braking',
       {
-        'bg-pri before:border-transparent after:translate-x-5 has-[:disabled]:bg-dis': modelValue,
-        'bg-bsc before:border-otl has-[:disabled]:before:border-dis': !modelValue
+        'bg-pri before:border-transparent after:translate-x-5 focus-within:after:!bg-pri/12 hover:after:bg-pri/8 has-[:disabled]:bg-dis':
+          modelValue,
+        'bg-bsc before:border-otl focus-within:after:!bg-on-bsc/12 hover:after:bg-on-bsc/8 has-[:disabled]:before:border-dis':
+          !modelValue
       }
     ]"
   >
     <input
-      :disabled
       type="checkbox"
-      v-model="modelValue"
+      :disabled
+      v-init
       @change="onChange"
-      class="pointer-events-none absolute inset-y-1 left-1 m-0 size-5 appearance-none rounded-inherit outline-none transition duration-300 ease-braking group-active/v-switch:scale-125"
+      class="pointer-events-none absolute inset-y-1 left-1 m-0 size-5 appearance-none rounded-inherit outline-none transition duration-inherit ease-braking group-active/v-switch:scale-125"
       :class="{
         'bg-on-pri disabled:bg-bsc': modelValue,
         'scale-75 bg-otl': !modelValue,
--- lib/components/switch/demo.vue
+++ lib/components/switch/demo.vue
@@ -3,11 +3,21 @@ import { ref } from 'vue'
 import { VSwitch } from '.'
 
 const value = ref(true)
+
+const value2 = ref()
 </script>
 
 <template>
-  <div class="flex gap-4">
-    <VSwitch v-model="value" />
-    <VSwitch v-model="value" disabled />
+  <div class="flex flex-col gap-2">
+    <div class="flex gap-2 font-mono">
+      <span>{{ `${value}` }}</span>
+      <span>{{ `${value2}` }}</span>
+    </div>
+
+    <div class="flex gap-4">
+      <VSwitch v-model="value" />
+      <VSwitch v-model="value2" />
+      <VSwitch v-model="value" disabled />
+    </div>
   </div>
 </template>
