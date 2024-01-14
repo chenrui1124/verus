4855044075d472a387f7bdaff59c583ec3682bc9
Rui <75087533+chenrui1124@users.noreply.github.com>
2024-01-14 20:35:35 +0800

    chore: update demo

--- lib/components/auto-grid/demo.vue
+++ lib/components/auto-grid/demo.vue
@@ -3,9 +3,7 @@ import { VAutoGrid } from '.'
 </script>
 
 <template>
-  <div class="w-80">
-    <VAutoGrid min-width="2.5rem">
-      <div class="size-10 bg-on-bsc" v-for="_ in 20" />
-    </VAutoGrid>
-  </div>
+  <VAutoGrid item-width="2.5rem" width="20rem">
+    <div class="size-10 bg-on-bsc" v-for="_ in 20" />
+  </VAutoGrid>
 </template>
--- lib/components/breadcrumbs/demo.vue
+++ lib/components/breadcrumbs/demo.vue
@@ -2,11 +2,17 @@
 import { ref } from 'vue'
 import { VBreadcrumbs } from '.'
 
+const each = [
+  { icon: 'i-ri-windows-fill', text: 'Windows', value: 'windows' },
+  { icon: 'i-ri-android-fill', text: 'Android', value: 'android' },
+  { icon: 'i-ri-apple-fill', text: 'IOS', value: 'ios' }
+] as const
+
 const modelValue = ref([])
 </script>
 
 <template>
   <div class="">
-    <VBreadcrumbs :each="['Windows', 'Android', 'IOS']" v-model="modelValue" selectable />
+    <VBreadcrumbs :each="each" v-model="modelValue" selectable />
   </div>
 </template>
