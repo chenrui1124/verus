994bcf5bf0b133dca10d31422b79ac9dd2f04b6a
Rui <75087533+chenrui1124@users.noreply.github.com>
2024-05-01 21:22:11 +0800

    chore: update deps

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,21 +2,6 @@
 export default {
   plugins: ['prettier-plugin-tailwindcss'],
   tailwindConfig: './pkgs/verus-ui/tailwind.config.ts',
-  tailwindAttributes: [
-    'class',
-    'enterFromClass',
-    'enterActiveClass',
-    'enterToClass',
-    'leaveFromClass',
-    'leaveActiveClass',
-    'leaveToClass',
-    'enter-from-class',
-    'enter-active-class',
-    'enter-to-class',
-    'leave-from-class',
-    'leave-active-class',
-    'leave-to-class'
-  ],
   bracketSameLine: false,
   jsxSingleQuote: true,
   printWidth: 100,
